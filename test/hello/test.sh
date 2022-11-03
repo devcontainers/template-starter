2c510733ae1505bbcc84cfed42d14e46061419d9
Samruddhi Khandale <skhandale@microsoft.com>
2022-11-03 23:06:19 +0000

    address comment

--- test/hello/test.sh
+++ test/hello/test.sh
@@ -4,7 +4,7 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "greeting" [ $(cat /tmp/greeting.txt | grep hey) ]
+check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
 
 # Report result
 reportResults
